5af6f4be5d69db096b1fe455d6be4af7cda3f60e
Zachary Owen <zowen@weblinc.com>
2013-12-26 11:07:39 -0500

    Revert "adds 1 day to pickup date"
    This reverts commit 09e5b8fb80226e1055d3734f4984fcfcacfc89e6.

--- lib/ups_time_in_transit.rb
+++ lib/ups_time_in_transit.rb
@@ -111,7 +111,7 @@ module Joestelmach
       #
       def request(options)
         # build our request xml
-        pickup_date = calculate_pickup_date + 1.day
+        pickup_date = calculate_pickup_date
         options[:pickup_date] = pickup_date.strftime('%Y%m%d')
         xml = @access_xml + generate_xml(build_transit_attributes(options))
 
